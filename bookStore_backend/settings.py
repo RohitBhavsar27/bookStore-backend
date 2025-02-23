9bb65bdb6b40442db8e23e472cd4a1df243fdd57
RohitBhavsar27 <bhavsar.rohit.m@gmail.com>
2025-02-23 15:56:40 +0530

    deploying to production

--- bookStore_backend/settings.py
+++ bookStore_backend/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i6wp$67)m^kw%4!$e&f8_*ldfleu6$@!#34l$s%@5+ir!(x$tr"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",  # Localhost (IP)
